d0e7492c7f8cbff4bb9619d1149d34dde7295c6b
Josh Wedekind 🥚 <josh@halfnibble.com>
2022-11-05 20:44:54 -0700

    Add MUI.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,101 @@
+import { useState, useRef } from "react";
+
+// MUI Components
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Drawer from "@mui/material/Drawer";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import Paper from "@mui/material/Paper";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+// MUI Icons
+import MenuIcon from "@mui/icons-material/Menu";
+
 import logo from "./logo.svg";
 import styles from "./App.module.scss";
 
-function App() {
+const App = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState<boolean>(false);
+  const containerRef = useRef(null);
+
+  const handleDrawerToggle = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
+  };
+
   return (
-    <div className={styles.App}>
-      <header className={styles.header}>
+    <Container ref={containerRef}>
+      <Box sx={{ display: "flex" }}>
+        <AppBar component="nav">
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: "none" } }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+            >
+              Cup O'Cheers
+            </Typography>
+            <Box sx={{ display: { xs: "none", sm: "block" } }}>
+              <Button key={"item1"} sx={{ color: "#fff" }}>
+                Item 1
+              </Button>
+            </Box>
+          </Toolbar>
+        </AppBar>
+        <Box component="nav">
+          <Drawer
+            container={containerRef.current}
+            variant="temporary"
+            open={mobileMenuOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+            sx={{
+              display: { xs: "block", sm: "none" },
+              "& .MuiDrawer-paper": { boxSizing: "border-box", width: 240 },
+            }}
+          >
+            <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+              <Typography variant="h6" sx={{ my: 2 }}>
+                Cup O'Cheers
+              </Typography>
+              <Divider />
+              <List>
+                <ListItem key={"item1"} disablePadding>
+                  <ListItemButton sx={{ textAlign: "center" }}>
+                    <ListItemText primary={"Item 1"} />
+                  </ListItemButton>
+                </ListItem>
+              </List>
+            </Box>
+          </Drawer>
+        </Box>
+      </Box>
+      <Paper className={styles.AppContent}>
         <img src={logo} className={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className={styles.link} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Placeholder for Cup O' Cheers!
-        </a>
-      </header>
-    </div>
+        <Typography sx={{ marginTop: "20px" }} variant={"h5"}>
+          Welcome to the temporary website for Cup O'Cheers!
+        </Typography>
+      </Paper>
+    </Container>
   );
-}
+};
 
 export default App;
